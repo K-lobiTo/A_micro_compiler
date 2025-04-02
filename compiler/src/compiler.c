588fdd5b0ad0824815138fadeff9a9a400d8ff93
K-lobiTo <calebam2112@gmail.com>
2025-04-02 00:59:41 -0500

    basic testing for scanner

--- compiler/src/compiler.c
+++ compiler/src/compiler.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../include/io_utils.h"
+#include "../include/scanner.h"
 
 
 int main() {
@@ -12,8 +13,21 @@ int main() {
         fprintf(stderr, "Error reading input\n");
         return 1;
     }
+    
 
     printf("%.*s\nRead %zu bytes\n", size, source, (int)size); // for testing [it worked]
+    Scanner sc;
+    scanner_init(&sc, source);
+    Token actToken = {TOKEN_ERROR, {0}};
+
+    while (actToken.type != TOKEN_EOF){ // for testing [works as expected]
+        actToken = next_token(&sc);
+        printf("TokenType %zu: Lexeme: %s\n", actToken.type, actToken.lexeme);
+    }
+    
     free(source);
     return 0;
+
+
+    // printf("\nbits %zu value\n", size,  ((2) < 8 ? ((1 << (2)) << 8) : ((1 << (2)) >> 8))); 
 }
