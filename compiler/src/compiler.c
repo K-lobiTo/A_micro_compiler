afeeda414860d5068c09eb1ff49975f80bd245d2
K-lobiTo <calebam2112@gmail.com>
2025-04-08 20:13:04 -0500

    testing ast

--- compiler/src/compiler.c
+++ compiler/src/compiler.c
@@ -1,33 +1,34 @@
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "../include/io_utils.h"
-#include "../include/scanner.h"
+#include "../include/parser.h"
 
-
-int main() {
-    size_t size;
-    char* source = read_stdin_to_end(&size);
-    if (!source) {
-        fprintf(stderr, "Error reading input\n");
-        return 1;
-    }
+int main(char* argv[]) {
+    Scanner scanner;
+    Trie trie;
+    Parser parser;
     
+    char* source = "begin; read b; read a; a = 5; b = a + 3; write b; end;";
 
-    printf("%.*s\nRead %zu bytes\n", size, source, (int)size); // for testing [it worked]
-    Scanner sc;
-    scanner_init(&sc, source);
-    Token actToken = {TOKEN_ERROR, {0}};
-
-    while (actToken.type != TOKEN_EOF){ // for testing [works as expected]
-        actToken = next_token(&sc);
-        printf("TokenType %s: Lexeme: %s\n", token_type_to_string(actToken.type), actToken.lexeme);
-    }
+    // char* source = argv[1];
+    // if (source == NULL) {
+    //     fprintf(stderr, "Error: No source code provided.\n");
+    //     return 1;
+    // }
     
-    free(source);
+    create_scanner(&scanner, source);
+    create_trie(&trie);
+    
+    trie_insert(&trie, "begin", false, TOKEN_BEGIN);
+    trie_insert(&trie, "end", false, TOKEN_END);
+    trie_insert(&trie, "read", false, TOKEN_READ);
+    trie_insert(&trie, "write", false, TOKEN_WRITE);
+    
+    init_parser(&parser, &scanner, &trie);
+    
+    ASTNode* ast = parse_program(&parser);
+    printf("\n--- AST ---\n");
+    print_ast(ast, 0);
+
+    //generate_code(ast);
+    
     return 0;
-
-
-    // printf("\nbits %zu value\n", size,  ((2) < 8 ? ((1 << (2)) << 8) : ((1 << (2)) >> 8))); 
 }
