b68aea8e67055e30708960958a8880812f2c0c98
K-lobiTo <calebam2112@gmail.com>
2025-04-08 20:05:35 -0500

    refactor (parser): Trie implemented, error language changed to Spanish

--- compiler/include/parser.h
+++ compiler/include/parser.h
@@ -2,14 +2,24 @@
 #define PARSER_H
 
 #include "scanner.h"
+#include "ast.h"
 
-typedef struct {
+typedef struct Parser {
     Scanner* scanner;
-    Token current_token;
+    Trie* trie;
+    Token current;
 } Parser;
 
-void parser_init(Parser* p, Scanner* s);
-void parse_program(Parser* p);
+void init_parser(Parser* p, Scanner* s, Trie* t);
 void match(Parser* p, TokenType expected_type);
+
+ASTNode* parse_program(Parser* p);
+ASTNode* parse_stmts(Parser* p);
+ASTNode* parse_stmt(Parser* p);
+ASTNode* parse_assignment(Parser* p);
+ASTNode* parse_read(Parser* p);
+ASTNode* parse_write(Parser* p);
+ASTNode* parse_expr(Parser* p);
+ASTNode* parse_term(Parser* p);
 
 #endif
--- compiler/src/parser.c
+++ compiler/src/parser.c
@@ -1,101 +1,114 @@
-#include "../../include/parser.h"
-#include "../../include/ast.h"
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "../include/parser.h"
 
-void parser_init(Parser* p, Scanner* s) {
-    p->scanner = s;
-    p->current_token = next_token(s);
-}
-
-void match(Parser* p, TokenType expected_type) {
-    if (p->current_token.type == expected_type) {
-        printf("Matched %s\n", token_type_to_string(expected_type));
-        p->current_token = next_token(p->scanner);
-    } else {
-        fprintf(stderr, "Syntax error: Expected %s, got %s\n",
-                token_type_to_string(expected_type),
-                token_type_to_string(p->current_token.type));
-        exit(1);
+void match(Parser* p, TokenType expected) {
+    while (p->current.type == TOKEN_COMMENT) p->current = next_token(p->scanner, p->trie);
+    if (p->current.type != expected) {
+        fprintf(stderr, "Error sintáctico: Esperaba %s, obtuvo %s\n",
+            token_type_to_string(expected), token_type_to_string(p->current.type));
+        fprintf(stderr, "Lexema: %s\n", p->current.lexeme);
+        exit(EXIT_FAILURE);
     }
-}
-            // char var[33];
-            // strcopy(var, p->current_token.lexeme);
-
-ASTNode* parse_statement(Parser* p) {
-    switch (p->current_token.type) {
-        case TOKEN_ID: {
-            char var_name[33];
-            strcpy(var_name, p->current_token.lexeme);
-            match(p, TOKEN_ID);
-            if(p->current_token.type == TOKEN_ASSIGN){ // Assignment (declaration + initialization)
-                match(p, TOKEN_ASSIGN);
-                ASTNode* expr = parse_expression(p);
-                match(p, TOKEN_SEMICOLON);
-                return ast_new_declaration(var_name, expr);
-            } else{// else: it's just declaration;
-                match(p, TOKEN_SEMICOLON);
-                return ast_new_declaration(var_name, NULL);
-            }
-        }
-        case TOKEN_READ: {
-            match(p, TOKEN_READ);
-            char var_name[33];
-            strcpy(var_name, p->current_token.lexeme);
-            match(p, TOKEN_ID);
-            match(p, TOKEN_SEMICOLON);
-            return ast_new_read(var_name);
-        }
-        case TOKEN_WRITE: {
-            match(p, TOKEN_WRITE);
-            ASTNode* expr = parse_expression(p);
-            match(p, TOKEN_SEMICOLON);
-            return ast_new_write(expr);
-        }
-        case TOKEN_COMMENT: {
-            match(p, TOKEN_COMMENT);
-            return NULL;
-        }
-        default:
-            fprintf(stderr, "Invalid statement at token%s\n", token_type_to_string(p->current_token.type));
-            exit(1);
-    }
+    p->current = next_token(p->scanner, p->trie);
 }
 
-ASTNode* parse_expression(Parser* p) {
-    ASTNode* left;
-    if (p->current_token.type == TOKEN_ID) {
-        left = ast_new_variable(p->current_token.lexeme);
-        match(p, TOKEN_ID);
+ASTNode* parse_program(Parser* p) {
+    match(p, TOKEN_BEGIN);
+    match(p, TOKEN_SEMICOLON);
+    ASTNode* stmts = parse_stmts(p);
+    match(p, TOKEN_END);
+    match(p, TOKEN_SEMICOLON);
+    match(p, TOKEN_EOF);
+    return create_program(stmts);
+}
+
+ASTNode* parse_stmts(Parser* p) {
+    if (p->current.type == TOKEN_ID || 
+        p->current.type == TOKEN_READ || 
+        p->current.type == TOKEN_WRITE) {
         
-    } else if(p->current_token.type == TOKEN_INT){
-        match(p, TOKEN_INT);
-    } else {
-        fprintf(stderr, "Invalid expression\n");
-        exit(1);
+        ASTNode* stmt = parse_stmt(p);
+        return create_stmt_seq(stmt, parse_stmts(p));
     }
-    if (p->current_token.type == TOKEN_PLUS) {
-        match(p, p->current_token.type);
-        ASTNode * right = parse_expression(p);
-        ast_add_binop(left, "+", right);
+    return NULL;
+}
+
+ASTNode* parse_stmt(Parser* p) {
+    switch(p->current.type) {
+        case TOKEN_ID:   return parse_assignment(p);
+        case TOKEN_READ: return parse_read(p);
+        case TOKEN_WRITE: return parse_write(p);
+        default:
+            fprintf(stderr, "Declaración inválida\n");
+            exit(EXIT_FAILURE);
+    }
+}
+
+ASTNode* parse_assignment(Parser* p) {
+    char* id = strdup(p->current.lexeme);
+    match(p, TOKEN_ID);
+    trie_insert(p->trie, id, true, TOKEN_ID);
+    match(p, TOKEN_ASSIGN);
+    ASTNode* expr = parse_expr(p);
+    match(p, TOKEN_SEMICOLON);
+    return create_assign(id, expr);
+}
+
+ASTNode* parse_read(Parser* p) {
+    match(p, TOKEN_READ);
+    char* id = strdup(p->current.lexeme);
+    match(p, TOKEN_ID);
+    trie_insert(p->trie, id, true, TOKEN_ID);
+    match(p, TOKEN_SEMICOLON);
+    return create_read(id);
+}
+
+ASTNode* parse_write(Parser* p) {
+    match(p, TOKEN_WRITE);
+    ASTNode* expr = parse_expr(p);
+    if (expr->type == ID_NODE) {
+        if (trie_search(p->trie, expr->id) == TOKEN_ERROR) {
+            fprintf(stderr, "Error: Variable '%s' no declarada\n", expr->id);
+            exit(EXIT_FAILURE);
+        }
+    }
+    match(p, TOKEN_SEMICOLON);
+    return create_write(expr);
+}
+
+ASTNode* parse_expr(Parser* p) {
+    ASTNode* left = parse_term(p);
+    while (p->current.type == TOKEN_PLUS) {
+        match(p, TOKEN_PLUS);
+        ASTNode* right = parse_term(p);
+        left = create_plus(left, right);
     }
     return left;
 }
 
-ASTNode* parse_program(Parser* p) {
-    ASTNode* program = ast_new_program();
-    while (p->current_token.type == TOKEN_COMMENT) {
-        match(p, TOKEN_COMMENT);
-    } 
-    match(p, TOKEN_BEGIN);
-    while (p->current_token.type != TOKEN_END) {
-        ASTNode* stmt = parse_statement(p);
-        if(stmt) ast_add_statement(program, stmt); //to skip NULL (comments)
+ASTNode* parse_term(Parser* p) {
+    if (p->current.type == TOKEN_ID) {
+        char* id = strdup(p->current.lexeme); 
+        if (trie_search(p->trie, id) == TOKEN_ERROR) {
+            fprintf(stderr, "Error: Variable '%s' no declarada\n", p->current.lexeme);
+            exit(EXIT_FAILURE);
+        }
+        match(p, TOKEN_ID);
+        return create_id(id);
+    } else if (p->current.type == TOKEN_VALUE) {
+        int value = atoi(p->current.lexeme);
+        match(p, TOKEN_VALUE);
+        return create_value(value);
+    } else {
+        fprintf(stderr, "Se esperaba identificador o número\n");
+        exit(EXIT_FAILURE);
     }
-    match(p, TOKEN_END);
-    while (p->current_token.type == TOKEN_COMMENT) {
-        match(p, TOKEN_COMMENT);
-    } 
-    return program;
+}
+
+void init_parser(Parser* p, Scanner* s, Trie* t) {
+    p->scanner = s;
+    p->trie = t;
+    p->current = next_token(s, t);
 }
